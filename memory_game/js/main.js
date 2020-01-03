a2d023c676bc9f313b597f895f3977c642b15141
JaAlSaDev <JaAlSaDev@gmail.com>
2020-01-03 18:48:52 +0300

    added arrays and alert messages.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards= ["queen", "queen", "king", "king"];
+const cardsInPlay=[];
+let cardOne=cards[0];
+let cardTwo=cards[1];
+let alertMessage;
 
-console.log("User flipped a "+cardOne);
-console.log("User flipped a "+cardThree);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+cardOne);
+console.log("User flipped "+cardTwo);
+
+
+
+if (cardsInPlay.length===2) {
+	
+	alertMessage=(cardsInPlay[0]===cardsInPlay[1])?"You found a match!":"Sorry, try again.";
+
+	alert(alertMessage);
+
+}
