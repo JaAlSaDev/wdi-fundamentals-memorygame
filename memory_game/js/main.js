e9eb36e1cb5f7d9ee37d15b4c40594ed7fa74a68
JaAlSaDev <JaAlSaDev@gmail.com>
2020-01-04 14:33:13 +0300

    added functions in main.js.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,27 @@
+//Global variables
 const cards= ["queen", "queen", "king", "king"];
 const cardsInPlay=[];
-let cardOne=cards[0];
-let cardTwo=cards[1];
-let alertMessage;
 
+//Functions
+function checkForMatch(){
+	let matchMessage;
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+	matchMessage=(cardsInPlay[0]===cardsInPlay[1])?"You found a match!":"Sorry, try again.";
 
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardTwo);
-
-
-
-if (cardsInPlay.length===2) {
-	
-	alertMessage=(cardsInPlay[0]===cardsInPlay[1])?"You found a match!":"Sorry, try again.";
-
-	alert(alertMessage);
-
+	console.log(matchMessage);
 }
+
+function flipCard(cardId){
+	cardsInPlay.push(cards[cardId]);
+
+	console.log("User flipped "+cards[cardId]);
+
+	if (cardsInPlay.length===2) {
+		checkForMatch();
+	}
+}
+
+
+//Main
+flipCard(0);
+flipCard(2);
