4516e025be4aaa4c1a34fb702d1092a59fa649ce
JaAlSaDev <JaAlSaDev@gmail.com>
2020-01-04 16:54:40 +0300

    added four card objects in main.js.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,25 @@
 //Global variables
-const cards= ["queen", "queen", "king", "king"];
+const queenHearts={
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "Images/queen-of-hearts.png"
+}
+const queenDiamonds={
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "Images/queen-of-diamonds.png"
+}
+const kingHearts={
+	rank: "king",
+	suit: "hearts",
+	cardImage: "Images/king-of-hearts.png"
+}
+const kingDiamonds={
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "Images/king-of-diamonds.png"
+}
+const cards= [queenHearts,queenDiamonds,kingHearts,kingDiamonds];
 const cardsInPlay=[];
 
 //Functions
@@ -8,14 +28,20 @@ function checkForMatch(){
 
 	matchMessage=(cardsInPlay[0]===cardsInPlay[1])?"You found a match!":"Sorry, try again.";
 
-	console.log(matchMessage);
+	alert(matchMessage);
+}
+
+function printCardInfo(cardId){
+	console.log("User flipped "+cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 function flipCard(cardId){
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped "+cards[cardId]);
-
+	printCardInfo(cardId);
+	
 	if (cardsInPlay.length===2) {
 		checkForMatch();
 	}
